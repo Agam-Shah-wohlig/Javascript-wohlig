62f273af092d7bfe4b05c3847088dba4516bbcb9
Agam Shah <agam.shah@wohlig.com>
2025-11-17 16:15:31 +0530

    update loops.js

--- loops.js
+++ loops.js
@@ -11,3 +11,14 @@ for (i=0;i<5;i++)
     document.getElementById("loops").innerHTML += star + "<br>";
 }
 
+
+outer: for(let i=0; i<3;i++){
+    console.log(i);
+    
+    for(let j=0;j<3;j++){
+        if(i===1 && j===1){
+            continue outer;
+        }
+        console.log(`i = ${i}, j = ${j}`);
+    }
+}
