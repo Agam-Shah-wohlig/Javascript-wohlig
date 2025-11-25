d168daac8e180b0f83fdc99a6f300d8e639f1603
Agam Shah <agam.shah@wohlig.com>
2025-11-25 12:16:45 +0530

    basic expressjs added

--- nodejs/http-server/index.js
+++ nodejs/http-server/index.js
@@ -3,25 +3,36 @@ const fs = require("fs");
 const url = require("url");
 
 const server = http.createServer((req, res)=>{
-    const log = `${Date.now()}: ${req.url} New Request Received\n`;
+    const log = `${Date.now()}: ${req.url} ${req.method} New Request Received\n`;
     const myUrl = url.parse(req.url, true);
     console.log(myUrl)
     fs.appendFile("./logs.txt", log, (err, data) => {
         switch(myUrl.pathname){
             case("/"): 
+                if (req.method === 'GET'){
                 res.end("You are at HomePage");
-            break
+            }
+            break;
             
             case("/about"): 
                 const username = myUrl.query.q; // /about?q=Agam
                 console.log(` The User is :${username}`)
                 res.end(" You are in about section");
-            break
+            break;
             
             case("/search"): 
                 const search = myUrl.query.search_query // /search?search_query=agam+sanjay+shah
                 res.end(`you search this query ${search}`);
-            break
+            break;
+
+            case("/signup"):
+                if (req.method === 'GET'){
+                    res.end('This is the signup form')
+                } else if(req.method === 'POST'){
+                    // DB Query
+                    res.end("Success")
+                }
+                break;
 
             default: res.end("This page Does not Exist");
         }
