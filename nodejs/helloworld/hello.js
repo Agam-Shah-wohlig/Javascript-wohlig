47ad63214e967dba8326620e611b1571b6dbfadf
Agam Shah <agam.shah@wohlig.com>
2025-12-22 16:11:52 +0530

    Remove unwanted nodejs folder

--- nodejs/helloworld/hello.js
+++
@@ -1,7 +0,0 @@
-console.log("hello world")
-
-// console.log(window) // This line won't work in Node js Runtime because it can only be run from Browser!
-                    // All the UI related work have been removed like navigator and all!
-                    // So all the functions in the window package will not run! like alert and other things!
-
-                    
